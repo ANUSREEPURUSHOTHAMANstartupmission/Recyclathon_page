9f34603e5a1c4833d6789a03b2c48bd62e582845
ANUSREEPURUSHOTHAMANstartupmission <121005558+ANUSREEPURUSHOTHAMANstartupmission@users.noreply.github.com>
2023-12-02 16:29:42 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ANUSREEPURUSHOTHAMANstartupmission.github.io',
   base:'/recyclathon',
-  integrations: [tailwind()]
+  integrations: [tailwind(), svelte()]
 });
